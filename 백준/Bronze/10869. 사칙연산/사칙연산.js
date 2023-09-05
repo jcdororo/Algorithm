8a310912949c0507c8ece1616b8fc2123c5001aa
Dororo <66819791+jcdororo@users.noreply.github.com>
2023-09-05 22:22:01 +0900

    [Bronze V] Title: 사칙연산, Time: 152 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.js
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -1,10 +1,11 @@
 let input = require('fs').readFileSync('dev/stdin').toString().split(' ');
 
-const num1 = Number(input[0]);
-const num2 = Number(input[1]);
+const a = Number(input[0]);
+const b = Number(input[1]);
 
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(Math.floor(num1 / num2));
-console.log(num1 % num2);
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(Math.floor(a/b));
+console.log(a%b);
