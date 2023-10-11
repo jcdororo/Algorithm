5dfb601bf8f340f924edab8785c5e3304b040635
Dororo <66819791+jcdororo@users.noreply.github.com>
2023-10-11 20:44:32 +0900

    [Silver V] Title: 단어 정렬, Time: 1376 ms, Memory: 19336 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -1,24 +1,35 @@
-// console.time();
-// /dev/stdin
-// ./input.txt
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+/* /dev/stdin */
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-
-
-const a = input[0];
-
-answer = [];
-
-
-for (let i = 1; i < input.length; i++) {
-  if(answer.indexOf(input[i]) === -1) {
-    answer.push(input[i]);
-  }
+let n = Number(input[0]);
+let arr = [];
+for(let i = 1; i <= n; i++) {
+  arr.push(input[i])
 }
 
-answer.sort((a, b) => a.localeCompare(b))
-      .sort((a, b) => a.length - b.length)
-      .forEach(i => console.log(i));
+const set = [...new Set(arr)]
+
+set.sort(compare);
+
+// console.log(set.join('\n'))
+for(let x of set) {
+  console.log(x);
+}
 
 
 
+function compare(a, b) {
+
+  if(a.length === b.length) {
+    if(a < b) {
+      return -1;
+    } else if(a > b) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  return a.length - b.length
+}
