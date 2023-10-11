f56fef1b64f2d0f22635aaf835a3a1536bb94e76
Dororo <66819791+jcdororo@users.noreply.github.com>
2023-10-11 20:05:30 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 560 ms, Memory: 64716 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,38 @@
+/* /dev/stdin */
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const count = parseInt(input[0]);
+const arrObj = [];
+let answer = '';
+
+for(let i = 1; i <= count; i++){
+  let [ a, b ] = input[i].split(' ')
+  arrObj.push([a, b]);
+}
+
+
+arrObj.sort(compare)
+
+
+for(let i = 0; i < arrObj.length; i++){
+  answer = answer + arrObj[i].join(' ') + '\n';
+}
+
+console.log(answer);
+
+
+
+
+
+
+
+function compare (a, b) {
+
+  if(a[1] == b[1]){
+    return a[0] - b[0];
+  }
+  
+  return a[1] - b[1]
+  
+}
