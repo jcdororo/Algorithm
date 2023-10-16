3d4131fa5f1fb88033b737b957a63135706c12d7
Dororo <66819791+jcdororo@users.noreply.github.com>
2023-10-16 19:42:56 +0900

    [Silver III] Title: 모든 순열, Time: 288 ms, Memory: 41092 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.js
@@ -0,0 +1,39 @@
+/* /dev/stdin */
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const n = Number(input[0]);
+let arr = [];
+for(let i = 0; i < n; i++) arr.push(i + 1);
+let tf = Array(n).fill(false);
+let selected = [];
+let answer = '';
+
+
+dfs(n, 0);
+console.log(answer);
+
+function dfs(m, deps) {
+  if(deps == m) {
+    let temp = [];
+    for(let x = 0; x < selected.length; x++) temp.push(arr[selected[x]])
+    for(let x = 0; x < temp.length; x++) answer += temp[x] + ' ';
+    
+    answer += '\n';
+    
+    
+  }
+
+  for(let i = 0; i < arr.length; i++) {
+    if(tf[i]) {
+      
+    } else {
+      selected.push(i);
+      tf[i] = true;
+      dfs(m, deps + 1);
+      selected.pop();
+      tf[i] = false;
+    }
+  }
+  
+}
