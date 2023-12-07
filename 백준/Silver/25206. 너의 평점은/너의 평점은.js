9d5e5c4135f768def8930a10e74052f0567cc08e
Dororo <66819791+jcdororo@users.noreply.github.com>
2023-12-07 11:52:13 +0900

    [Silver V] Title: 너의 평점은, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.js
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.js
@@ -1,29 +1,34 @@
 /* /dev/stdin */
 let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(item=>item.split(' '));
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let sum = 0; 
-let total = 0;
-
-const table = {
-	"A+": 4.5, A0: 4.0,
-	"B+": 3.5, B0: 3.0,
-	"C+": 2.5, C0: 2.0,
-	"D+": 1.5, D0: 1.0,
-	F: 0.0
+const maps = {
+  'A+':4.5,
+  'A0':4.0,
+  'B+':3.5,
+  'B0':3.0,
+  'C+':2.5,
+  'C0':2.0,
+  'D+':1.5,
+  'D0':1.0,
+  'F' :0.0,
 }
 
+let x = 0; //(학점 * maps)
+let sum = 0;
 
-
-for(let [sub, score, grade] of input) {  
-
-  //grade = grade.replace('\r','')
-
-  if(grade==='P') {
-    continue;
-  }
-  sum += Number(score)*table[grade] // 학점에 따라서 key 값으로 table에서 빼와서 계산
-  total += Number(score); // total 값 저장
+for(let i = 0; i < input.length; i++) {
+  let [a, myX, myMaps] = input[i].split(' ');
   
+
+  //myMaps = myMaps.replace('\r','')
+  myX = Number(myX);
+
+  if(myMaps == 'P') {
+
+  } else {
+    x += (myX * maps[myMaps]);
+    sum += myX;
+  }
 }
-console.log((sum / total));
+console.log((x / sum).toFixed(6));
