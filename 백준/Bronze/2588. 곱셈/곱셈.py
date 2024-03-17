4cd38d32ff278597c16f1afec173c8913b70bacd
jichan <66819791+jcdororo@users.noreply.github.com>
2024-03-17 16:54:13 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,9 @@
+import sys
+
+n = sys.stdin.readline().strip()
+m = sys.stdin.readline().strip()
+
+print(int(n) * int(m[2]))
+print(int(n) * int(m[1]))
+print(int(n) * int(m[0]))
+print(int(n) * int(m))
