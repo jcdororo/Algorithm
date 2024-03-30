9a7a21558e515fdd4a258872e86a34c0b5867e23
jichan <66819791+jcdororo@users.noreply.github.com>
2024-03-30 16:16:11 +0900

    [Silver I] Title: 추월, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2002. 추월/추월.py
+++ 백준/Silver/2002. 추월/추월.py
@@ -1,18 +1,33 @@
-from collections import deque
+import sys
 
-answer = 0
-n = int(input())
-q1 = deque()
+N = int(sys.stdin.readline().strip())
 
-for i in range(n*2):
-    if i < n:
-        q1.append(input())
-    else:
-        out = input()
-        if out != q1[0]:
-            q1.remove(out)
-            answer += 1
-        else:
-            q1.popleft()
+inCar = {}
+outCar = {}
 
-print(answer)
+result = 0
+
+for i in range(N * 2):
+  name = sys.stdin.readline().strip()
+  if i < N:
+    inCar[name] = i
+  else:
+    outCar[name] = i - N
+
+
+# for key, value in inCar.items():
+#   if value > outCar[key]:
+#     result += 1
+    
+# print(result)
+    
+out_keys = list(outCar.keys())
+# 지금 나온 차의 들어간 순서보다 나중에 나온 차의 들어간 순서가 앞인 경우에는 추월
+for i in range(0, len(out_keys)-1):
+    now_in = inCar[out_keys[i]]
+    for j in range(i+1, len(out_keys)):
+        next_in = inCar[out_keys[j]]
+        if next_in < now_in:
+            result += 1
+            break
+print(result)
