6b06e728b1fe2eb2e1054ff77701958f64eee6a1
jichan <66819791+jcdororo@users.noreply.github.com>
2024-03-30 16:48:29 +0900

    [Silver I] Title: 추월, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2002. 추월/추월.py
+++ 백준/Silver/2002. 추월/추월.py
@@ -15,19 +15,16 @@ for i in range(N * 2):
     outCar[name] = i - N
 
 
-# for key, value in inCar.items():
-#   if value > outCar[key]:
-#     result += 1
-    
-# print(result)
-    
 out_keys = list(outCar.keys())
-# 지금 나온 차의 들어간 순서보다 나중에 나온 차의 들어간 순서가 앞인 경우에는 추월
-for i in range(0, len(out_keys)-1):
-    now_in = inCar[out_keys[i]]
-    for j in range(i+1, len(out_keys)):
-        next_in = inCar[out_keys[j]]
-        if next_in < now_in:
-            result += 1
-            break
+for i in range(0, len(out_keys) - 1):
+  nowOut = inCar[out_keys[i]]
+
+  for j in range(i + 1, len(out_keys)):
+    nextOut = inCar[out_keys[j]]
+    if nextOut < nowOut:
+      result += 1
+      break
+
+
 print(result)
+
