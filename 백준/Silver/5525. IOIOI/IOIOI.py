30dd367361d65a29c0c594404688ad0b349062a1
jichan <66819791+jcdororo@users.noreply.github.com>
2024-03-30 19:05:16 +0900

    [Silver I] Title: IOIOI, Time: 512 ms, Memory: 35232 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,19 +1,30 @@
+# 4일차 - 7번 IOIOI
+# 일단 이전 문자랑 현재 문자가 같다 = x
+# I로 시작해야된다.
+# I의 개수는 N+1, O의 개수는 N
+# 문자열을 한번 돌면서, I의 개수 누적, O의 개수 누적. 같은 문자열이 나오면 reset
 import sys
+from collections import defaultdict
+input = sys.stdin.readline
+N = int(input())
+M = int(input())
+S = input().rstrip()
+cNum = defaultdict(int)
+count = 0
+for i, c in enumerate(S) :
+    # 연속으로 같은 문자열이 들어온 경우.
+    if i > 0 and c == S[i-1] :
+        cNum['I'] = c == 'I' and 1 or 0
+        cNum['O'] = 0
+        continue
 
-N = int(sys.stdin.readline().strip())
-lenS = int(sys.stdin.readline().strip())
-S = sys.stdin.readline().strip()
+    if c == 'I' :
+        cNum['I'] += 1
+        if cNum['I'] == N+1 and cNum['O'] == N :
+            count +=1
+            cNum['I'] -= 1
+            cNum['O'] -= 1
+    elif cNum['I'] > 0 and c == 'O' :
+        cNum['O'] += 1
 
-IOI = 'IOI'
-
-result = 0
-
-if N >= 2:
-  for _ in range(1,N):
-    IOI += 'OI'
-
-for i in range(lenS):
-  if S[i:i+len(IOI)] == IOI:
-    result += 1
-
-print(result)
+print(count)
