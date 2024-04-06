0649b3d7907f44180c6c8e5a56f88d5b7c2dc4c3
jichan <66819791+jcdororo@users.noreply.github.com>
2024-04-06 10:44:33 +0900

    [Silver III] Title: 통계학, Time: 584 ms, Memory: 52504 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,52 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+arr = [int(sys.stdin.readline().strip()) for _ in range(N)]
+arr.sort()
+
+# 산술평균
+def average(arr):
+  # print('avg', round(sum(arr) / len(arr)))
+
+  # return abs(round(sum(arr) / len(arr)))
+  return round(sum(arr) / len(arr))
+
+# 중앙값
+def middle(arr):
+  temp = arr
+  left = 0
+  right = len(temp) - 1
+  mid = (left + right) // 2
+
+  # return temp[mid]
+  return temp[len(temp)//2]
+
+# 최빈값
+def maxCount(arr):
+  dic = {}
+  for v in arr:
+    if dic.get(str(v), False):
+      dic[str(v)] += 1
+    else:
+      dic[str(v)] = 1
+
+  maxCount = max(dic.values())
+
+
+  temp = []
+  for k, v in dic.items():
+    if v == maxCount:
+      temp.append(int(k))
+  # print('temp',temp)
+
+  return temp[1] if len(temp) > 1 else temp[0]
+
+# 범위
+def numRange(arr):
+
+  return max(arr) - min(arr)
+
+print(average(arr))
+print(middle(arr))
+print(maxCount(arr))
+print(numRange(arr))
