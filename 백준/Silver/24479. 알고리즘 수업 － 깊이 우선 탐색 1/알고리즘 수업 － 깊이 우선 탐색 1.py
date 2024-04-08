bbd590fb7d1e02b4927a18fbbcc34b0e9a7fe555
jichan <66819791+jcdororo@users.noreply.github.com>
2024-04-08 11:06:31 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 616 ms, Memory: 68780 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,37 @@
+import sys
+from sys import setrecursionlimit
+setrecursionlimit(10 ** 6)
+
+
+N, M, R= map(int, sys.stdin.readline().strip().split(' '))
+
+graph = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
+race = [0 for _ in range(N + 1)]
+cnt = 1
+
+for i in range(M):
+  u, v = map(int, sys.stdin.readline().strip().split(' '))
+  graph[u].append(v)
+  graph[v].append(u)
+
+
+def dfs(node):
+  global cnt
+  visited[node] = True
+  graph[node].sort()
+  race[node] = cnt
+  
+  for i in graph[node]:
+    if not visited[i]:
+      cnt += 1
+      dfs(i)
+      
+
+dfs(R)
+
+# print(race)
+for i in range(1, len(race)):
+  print(race[i])
+
+
