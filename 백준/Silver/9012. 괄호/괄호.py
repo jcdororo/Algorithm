413a417c1da2b7f24ce831b3103b35f01fb4a058
jichan <66819791+jcdororo@users.noreply.github.com>
2024-04-10 17:33:16 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,23 +1,29 @@
 import sys
 
-N = int(sys.stdin.readline().strip())
 
-for _ in range(N):
-  word = sys.stdin.readline().strip()
+T = int(sys.stdin.readline().strip())
+
+for _ in range(T):
+  word = list(sys.stdin.readline().strip())
   stack = []
   answer = 'YES'
-  for w in word:
-    if w == '(':
+
+
+  for s in word:
+    if s == '(':
       stack.append('(')
-    elif w== ')':
-      if '(' in stack:
+    elif s == ')':
+      if stack and stack[-1] == '(':
         stack.pop()
-      else:
+      elif len(stack) == 0:
         answer = 'NO'
         break
-
+      
   if len(stack) > 0:
     answer = 'NO'
   print(answer)
-  
-  
+
+      
+
+
+  # break
