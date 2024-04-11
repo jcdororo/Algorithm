179dbae1b4e4cc0629615019bce0d19788cbb952
jichan <66819791+jcdororo@users.noreply.github.com>
2024-04-11 14:33:05 +0900

    [Silver II] Title: 나무 자르기, Time: 2136 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,25 +1,38 @@
 import sys
 
-N, M = map(int, (sys.stdin.readline().strip().split(' ')))
-arr = list(map(int, (sys.stdin.readline().strip().split(' '))))
 
+N, M = map(int, sys.stdin.readline().strip().split(' '))
+trees = list(map(int, sys.stdin.readline().strip().split(' ')))
+
+# print(trees)
 def search(arr, target):
   left = 0
   right = max(arr)
-
+  result = 0
   while left <= right:
     mid = (left + right) // 2
+    # temp = [x for x in arr]
+    # for i in range(len(temp)):
+    #   if temp[i] - mid >= 0:
+    #     temp[i] -= mid
+    #   else:
+    #     temp[i] = 0
+    # print(temp)    
+        
+    # print(temp, mid, left, right)
     total = 0
-
     for x in arr:
       if x >= mid:
         total += (x - mid)
 
     if total >= target:
-      left = (mid + 1)
+      result = mid
+      left = mid + 1
     else:
-      right = (mid - 1)
+      right = mid - 1
 
-  return right
+  return result
 
-print(search(arr, M))
+print(search(trees, M))
+
+
