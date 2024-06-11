3db2467453baed33fb21772085ed623b16db0684
jichan <66819791+jcdororo@users.noreply.github.com>
2024-06-11 13:05:23 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
@@ -1,17 +1,49 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+// /dev/stdin
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number)
 
-function findDwarfs() {
-    for (let i = 0; i < input.length; i++) {
-        for (let j = i + 1; j < input.length; j++) {
-            for (let k = j + 1; k < input.length; k++) {
-                for (let l = k + 1; l < input.length; l++) {
-                    for (let m = l + 1; m < input.length; m++) {
-                        for (let n = m + 1; n < input.length; n++) {
-                            for (let o = n + 1; o < input.length; o++) {
-                                const dwarfs = [input[i], input[j], input[k], input[l], input[m], input[n], input[o]];
-                                if (dwarfs.reduce((sum, height) => sum + height, 0) === 100) {
-                                    return dwarfs.sort((a, b) => a - b);
+// let visited = []
+// for(let i = 0; i < 7; i++) {
+//     visited.push(false)
+// }
+// console.log(input, visited)
+
+// const arr = []
+// function dfs(depth) {
+//     if(arr.length === 7) {
+//         console.log(arr)
+//         return
+//     }
+
+//     for(let i = 0; i < input.length; i++) {
+//         if(!visited[i]) {
+//             visited[i] = true;
+//             arr.push(input[i])
+//             dfs(depth + 1)
+//             arr.pop()
+//             visited[i] = false;
+//         }
+//     }
+// }
+
+// dfs(0)
+
+
+
+
+
+
+
+function result () {
+    for(let q = 0; q < input.length; q++) {
+        for(let w = q+1; w < input.length; w++) {
+            for(let e = w+1; e < input.length; e++) {
+                for(let r = e+1; r < input.length; r++) {
+                    for(let a = r+1; a < input.length; a++) {
+                        for(let s = a+1; s < input.length; s++) {
+                            for(let d = s+1; d < input.length; d++) {
+                                const arr = [input[q],input[w],input[e],input[r],input[a],input[s],input[d]];
+                                if (arr.reduce((acc, cur) => acc + cur, 0) === 100) {
+                                    return arr.sort((a, b) => a - b);
                                 }
                             }
                         }
@@ -21,8 +53,5 @@ function findDwarfs() {
         }
     }
 }
-
-const answer = findDwarfs();
-if (answer) {
-    answer.forEach(height => console.log(height));
-}
+const answer = result();
+answer.forEach((height) => console.log(height));
