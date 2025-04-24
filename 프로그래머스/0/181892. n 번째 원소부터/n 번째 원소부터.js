91f50d8d4d4287dd4c0d9a8c1d5bf9a11a8e6803
jichan <66819791+jcdororo@users.noreply.github.com>
2025-04-24 19:22:24 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.js
@@ -0,0 +1,10 @@
+function solution(num_list, n) {
+    var answer = [];
+    
+    for(let i = n - 1; i < num_list.length; i++) {
+        
+        answer.push(num_list[i])
+    }
+    
+    return answer;
+}
