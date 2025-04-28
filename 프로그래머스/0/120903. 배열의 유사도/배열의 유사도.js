e9c106ea1940d9914a1209c906bea4b07c0ed495
jichan <66819791+jcdororo@users.noreply.github.com>
2025-04-28 18:59:06 +0900

    [level 0] Title: 배열의 유사도, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -0,0 +1,11 @@
+function solution(s1, s2) {
+    let answer = 0;
+    
+    for(let i = 0; i < s1.length; i++) {
+        if (s2.find(num => num == s1[i])) {
+            answer++
+        }
+    }
+    
+    return answer;
+}
