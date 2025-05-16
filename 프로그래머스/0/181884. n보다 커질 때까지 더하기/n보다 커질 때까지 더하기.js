5003ca1bced594da438b7ddf5a95bcc4a1cb4d52
jichan <66819791+jcdororo@users.noreply.github.com>
2025-05-16 16:27:01 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,16 @@
+function solution(numbers, n) {
+    var answer = 0;
+    
+    
+    for(number of numbers) {
+        answer += number
+        
+        if(answer > n) {
+            break
+        }
+    }    
+        
+        
+    
+    return answer;
+}
