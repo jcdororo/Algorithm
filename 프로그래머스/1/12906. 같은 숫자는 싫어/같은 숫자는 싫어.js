d200036f08b69d9fcb718b49382134d0a2ef7bbb
jichan <66819791+jcdororo@users.noreply.github.com>
2025-05-23 20:49:33 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 38.83 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,15 @@
+function solution(arr)
+{
+    var answer = [];
+    
+    for(let i = 0 ; i < arr.length; i++) {
+        if(answer[answer.length - 1] === arr[i]) {
+            continue;
+        } else {
+            answer.push(arr[i])
+        }
+    }
+
+    
+    return answer;
+}
