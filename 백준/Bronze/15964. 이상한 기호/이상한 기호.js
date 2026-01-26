b29423b8d343811ccce2c294f3d30722d3512cf6
jichan <66819791+jcdororo@users.noreply.github.com>
2026-01-26 21:36:02 +0900

    [Bronze V] Title: 이상한 기호, Time: 92 ms, Memory: 9340 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.js
@@ -0,0 +1,10 @@
+// /dev/stdin
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+
+const result = (A + B) * (A - B);
+
+console.log(result);
