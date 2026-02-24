a1b7fce26598875d12f5d883da4f67d2e16ec74d
jichan <66819791+jcdororo@users.noreply.github.com>
2026-02-24 12:25:30 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 960 ms, Memory: 151616 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -1,55 +1,60 @@
-// /dev/stdin
-const fs = require("fs");
+/* /dev/stdin */
+let fs = require("fs");
 let input = fs
   .readFileSync("/dev/stdin")
   .toString()
   .split("\n")
-  .map((x) => x.replace("\r", ""));
+  .map((x) => x.replaceAll("\r", ""));
 
 const N = Number(input[0]);
-const numberCards = input[1].split(" ").map(Number);
-const M = Number(input[2]);
-let myCards = input[3]
+const myCards = input[1]
   .split(" ")
   .map(Number)
   .sort((a, b) => a - b);
 
-const maps = new Map();
+const M = Number(input[2]);
+const targetCards = input[3].split(" ").map(Number);
+const answer = [];
 
-for (const key of myCards) {
-  maps[key] = 0;
+for (const targetCard of targetCards) {
+  const lower = lowerBound(myCards, targetCard);
+  const upper = upperBound(myCards, targetCard);
+
+  const result = upper - lower;
+  answer.push(result);
 }
+console.log(answer.join(" "));
 
-let answer = "";
-
-for (const x of numberCards) {
-  const result = binarySearch(myCards, x);
-  if (result === 1) {
-    maps[x]++;
-  }
-}
-
-function binarySearch(cards, target) {
+function lowerBound(myCards, targetCard) {
   let left = 0;
-  let right = cards.length - 1;
+  let right = myCards.length;
 
-  while (left <= right) {
+  while (left < right) {
     const mid = Math.floor((left + right) / 2);
 
-    if (cards[mid] === target) {
-      return 1;
-    } else if (cards[mid] < target) {
-      left = mid + 1;
+    if (myCards[mid] >= targetCard) {
+      right = mid;
     } else {
-      right = mid - 1;
+      left = mid + 1;
     }
   }
 
-  return 0;
-}
-myCards = input[3].split(" ").map(Number);
-for (const number of myCards) {
-  answer += maps[number] + " ";
+  return left;
 }
 
-console.log(answer);
+function upperBound(myCards, targetCard) {
+  let left = 0;
+  let right = myCards.length;
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (myCards[mid] > targetCard) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+}
