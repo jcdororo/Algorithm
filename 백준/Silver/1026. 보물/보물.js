24494dcf8896554b355f5c36a616022b08fe0f52
jichan <66819791+jcdororo@users.noreply.github.com>
2026-03-25 11:52:13 +0900

    [Silver IV] Title: 보물, Time: 92 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.js
+++ 백준/Silver/1026. 보물/보물.js
@@ -1,15 +1,24 @@
-/* /dev/stdin */
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+// /dev/stdin
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((x) => x.replace("\r", ""));
 
 const N = Number(input[0]);
-let A = input[1].split(' ').map(Number).sort((a,b) => a-b);
-let B = input[2].split(' ').map(Number).sort((a,b) => b-a);
-let result = 0;
+const A = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+const B = input[2]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => b - a);
 
+let answer = 0;
 
-for(let i = 0; i < N; i++){
-  result += A[i] * B[i];
+for (let i = 0; i < N; i++) {
+  answer += A[i] * B[i];
 }
-
-console.log(result);
+console.log(answer);
